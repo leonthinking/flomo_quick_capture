f307d0ca99116441344d0d12eb81c99b82cf2e82
Leon <leonmail163@163.com>
2025-02-12 10:55:05 +0800

    Delete Chrome Plugin directory

--- Chrome Plugin/bilibili 截屏/background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.type === 'download' && request.data) {
-        chrome.downloads.download({
-            url: request.data.url,
-            filename: request.data.filename,
-            saveAs: false
-        });
-    }
-});
